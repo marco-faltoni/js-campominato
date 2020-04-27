19ab29ea0264dc625514632ac5763eceec2af434
Marco <marco.faltoni@outlook.it>
2020-04-27 15:25:38 +0200

    ES

--- main.js
+++ main.js
@@ -5,6 +5,14 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito; in altre parole, deve comunicare all'utente quante giocate ha fatto prima di perdere.
 
 // Il computer deve generare 16 numeri casuali tra 1 e 100, che saranno le "mine".
+function generaRandom(min, max) {
+    var numero = Math.floor(Math.random() * (max - min + 1)) + min;
+    return numero;
+}
+
+var giocata_pc = generaRandom(1, 100);
+
+
 function generaRandom(numeri) {
     var numeri = [];
     while(numeri.length < 16){
@@ -14,8 +22,7 @@ function generaRandom(numeri) {
     console.log(numeri);
 }
 
-var giocata_pc = generaRandom(1, 200);
-console.log(giocata_pc);
+
 
 
 // In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
