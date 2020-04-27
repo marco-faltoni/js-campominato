d505adbc2e9ebc4231b3b0c12f62d23c2696ceb3
Marco <marco.faltoni@outlook.it>
2020-04-27 16:47:19 +0200

    Manca ultimo step

--- main.js
+++ main.js
@@ -34,7 +34,13 @@ do {
         console.log('Hai perso, hai preso una mina: le tue giocate sono state ' + (giocata_pc - giocata_utente) );
     }
 } while(isNaN(giocata_utente) || giocata_utente < 1 || giocata_utente > 100 || num.length > 84);
-console.log('Complimenti! sei riuscito a battere il Computer e a sviare tutte le mine.');
+
+if (isNaN(giocata_utente) || giocata_utente < 1 || giocata_utente > 100 || num.length > 84) {
+    console.log('Hai barato, ricarica la pagina e gioca ancora.');
+} else {
+    console.log('Complimenti! sei riuscito a battere il Computer e a sviare tutte le mine.');
+}
+
 
 // Se il numero è presente nella lista delle mine, la partita termina, altrimenti il gioco continua chiedendo all'utente un altro numero (continua a giocare).
 
