db7bf6ac5c96b1cb0379a99db44377043702d4bf
Marco <marco.faltoni@outlook.it>
2020-04-27 15:15:51 +0200

    ES

---
+++ main.js
@@ -0,0 +1,31 @@
+// Il computer deve generare 16 numeri casuali tra 1 e 100, che saranno le "mine".
+// In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
+// Se il numero è presente nella lista delle mine, la partita termina, altrimenti il gioco continua chiedendo all'utente un altro numero (continua a giocare).
+// La partita termina quando il giocatore becca una mina, ossia inserisce un numero "vietato", oppure se raggiunge il numero massimo possibile di numeri consentiti, ossia ha inserito tutti i numeri possibili che non sono mine!
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito; in altre parole, deve comunicare all'utente quante giocate ha fatto prima di perdere.
+
+// Il computer deve generare 16 numeri casuali tra 1 e 100, che saranno le "mine".
+function generaRandom(numeri) {
+    var numeri = [];
+    while(numeri.length < 16){
+        var r = Math.floor(Math.random() * 100) + 1;
+        if(numeri.indexOf(r) === -1) numeri.push(r);
+    }
+    console.log(numeri);
+}
+
+var giocata_pc = generaRandom(1, 200);
+console.log(giocata_pc);
+
+
+// In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
+
+
+// Se il numero è presente nella lista delle mine, la partita termina, altrimenti il gioco continua chiedendo all'utente un altro numero (continua a giocare).
+
+
+// La partita termina quando il giocatore becca una mina, ossia inserisce un numero "vietato", oppure se raggiunge il numero massimo possibile di numeri consentiti (84!)
+
+
+
+// Al termine della partita il software deve comunicare all'utente quante giocate ha fatto prima di perdere.
